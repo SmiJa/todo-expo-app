e738ad08215a0264dc6c3884e0e501ad0bd47466
Jason <smija13@gmail.com>
2021-10-07 09:12:22 -0700

    Fix RegEx

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useRef } from 'react';
-import { StyleSheet, Text, View, ScrollView, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TextInput, Button, CheckBox } from 'react-native';
+
 
 export default function App() {
 	const [items, setItems] = useState([
@@ -11,13 +12,20 @@ export default function App() {
 
 	const generateList = items.map((item, index) => (
 		<View key={index} style={styles.listItemContainer}>
-		<Text style={styles.item}>{item.todo}</Text>
+      <Text style={styles.item}>
+        <CheckBox
+          style={styles.checkBox}
+          key={index}
+          onValueChange={() => removeItemFromList(index)}
+        />
+        {item.todo}
+      </Text>
 		</View>
 	));
 
   const checkIfSame = () => {
     for (let i = 0; i < items.length; i++) {
-      if (items[i].todo.toLocaleLowerCase() === newItemText.toLocaleLowerCase()) {
+      if (items[i].todo.toLocaleLowerCase().trim() === newItemText.toLocaleLowerCase().trim()) {
         return true;
       } else {
         return false;
@@ -25,17 +33,18 @@ export default function App() {
     }
   }
 
-  // const removeItemFromList = (task) => {
-  //   setItems(items.filter((value, index) => index != task))
-  // }
+  const removeItemFromList = (task) => {
+    setItems(items.filter((value, index) => index != task));
+    console.log(task.todo + " has been removed.");
+  };
 
 	const addToList = () => {
-		setItems([...items, {todo: newItemText}]);
+		setItems([...items, {todo: newItemText.trim()}]);
 		setNewItemText("");
 	}
 
 	const checkIfValid = () => {
-		if (newItemText.length > 0 && isNaN(newItemText)) {
+		if (newItemText.trim().length > 0 && isNaN(newItemText.trim()) && /[a-zA-Z0-9\g]/.test(newItemText.trim())) {
 			console.log(newItemText);
       if (checkIfSame()) {
         console.log(newItemText + " already exists.");
@@ -53,9 +62,9 @@ return (
 	<Text style={styles.header}>Todo List</Text>
     <View style={styles.inputWrap}>
         <TextInput
-			style={styles.input}
-			onChangeText = {text => setNewItemText(text)}
-			value = {newItemText}
+          style={styles.input}
+          onChangeText = {text => setNewItemText(text)}
+          value = {newItemText}
         />
         <View style={styles.btnWrap}>
         <Button
@@ -131,5 +140,8 @@ const styles = StyleSheet.create({
   btnWrap: {
     width: '70%',
     margin: 'auto'
+  },
+  checkBox: {
+    marginRight: 20,
   }
 });
